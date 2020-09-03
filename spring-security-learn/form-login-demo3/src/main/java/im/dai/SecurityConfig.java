4d76ed7611aa91cdf1c5607901390445add1d6e3
roc <daiim@qq.com>
2020-09-03 11:42:40 +0800

    用户授权

---
+++ spring-security-learn/form-login-demo3/src/main/java/im/dai/SecurityConfig.java
@@ -0,0 +1,134 @@
+package im.dai;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import java.io.PrintWriter;
+
+/**
+ * @ClassName SecurityConfig
+ * @author: roc
+ * @date: 2020/9/2 下午 03:51
+ * @version: 1.0
+ * @description: 方案二：配置类
+ * BCryptPasswordEncoder 使用 BCrypt 强哈希函数，开发者在使用时可以选择提供 strength 和 SecureRandom 实例。
+ * strength 越大，密钥的迭代次数越多，密钥迭代次数为 2^strength。strength 取值在 4~31 之间，默认为 10。
+ * 相对比 shiro 需要自己处理密码加盐，BCryptPasswordEncoder 就自带了盐。
+ */
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+        //return new BCryptPasswordEncoder();
+    }
+
+    //@Override
+    //protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    //    auth.inMemoryAuthentication()
+    //            .withUser("admin")
+    //            .password("admin")
+    //            //.password(new BCryptPasswordEncoder().encode("admin"))
+    //            .roles("admin");
+    //}
+
+    /**
+     *  通过重写 WebSecurityConfigurerAdapter 中的 userDetailsService 方法
+     *  来提供一个 UserDetailService 实例进而配置多个用户
+     */
+    @Bean
+    protected UserDetailsService userDetailsService() {
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(User.withUsername("admin").password("admin").roles("admin").build());
+        manager.createUser(User.withUsername("user").password("user").roles("user").build());
+        return manager;
+    }
+
+    /**
+     * 角色继承
+     */
+    @Bean
+    RoleHierarchy roleHierarchy() {
+        RoleHierarchyImpl hierarchy = new RoleHierarchyImpl();
+        hierarchy.setHierarchy("ROLE_admin > ROLE_user");
+        return hierarchy;
+    }
+
+    /**
+     * 自定义登录页面
+     */
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        //web.ignoring() 用来配置忽略掉的 URL 地址，一般对于静态文件
+        web.ignoring().antMatchers("/js/**","/css/**","/images/**");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                // 以下为拦截规则，有顺序要求
+                .antMatchers("/admin/**").hasRole("admin")
+                .antMatchers("/user/**").hasRole("user")
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login.html")
+                .loginProcessingUrl("/doLogin")
+                .usernameParameter("name")
+                .passwordParameter("pwd")
+                //successHandler 已经囊括了 defaultSuccessUrl 和 successForwardUrl 的功能
+                .successHandler((req, resp, authentication) -> {
+                    Object principal = authentication.getPrincipal();
+                    resp.setContentType("application/json;charset=utf-8");
+                    PrintWriter out = resp.getWriter();
+                    out.write(new ObjectMapper().writeValueAsString(principal));
+                    out.flush();
+                    out.close();
+                })
+                .failureHandler((req, resp, e) -> {
+                    resp.setContentType("application/json;charset=utf-8");
+                    PrintWriter out = resp.getWriter();
+                    out.write(e.getMessage());
+                    out.flush();
+                    out.close();
+                })
+                .permitAll()
+                .and()
+                .logout()
+                .logoutUrl("/logout")
+                .logoutSuccessHandler((req, resp, authentication) -> {
+                    resp.setContentType("application/json;charset=utf-8");
+                    PrintWriter out = resp.getWriter();
+                    out.write("注销成功");
+                    out.flush();
+                    out.close();
+                })
+                .permitAll()
+                .and()
+                .csrf().disable().exceptionHandling()
+                .authenticationEntryPoint((req, resp, authException) -> {
+                            resp.setContentType("application/json;charset=utf-8");
+                            PrintWriter out = resp.getWriter();
+                            out.write("尚未登录，请先登录");
+                            out.flush();
+                            out.close();
+                        }
+                );
+    }
+
+}
