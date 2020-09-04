9f078f3dced64b98bc61762e04776814bb42783a
roc <daiim@qq.com>
2020-09-04 17:05:10 +0800

    引入验证码完成自定义认证逻辑

---
+++ spring-security-learn/captcha/src/main/java/im/dai/controller/HelloController.java
@@ -0,0 +1,19 @@
+package im.dai.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @ClassName HelloController
+ * @author: roc
+ * @date: 2020/9/4 下午 04:46
+ * @version: 1.0
+ * @description: TODO
+ */
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+}
