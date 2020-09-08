b45711c4a697593b581ae0e82c83cd77237649fb
roc <daiim@qq.com>
2020-09-08 15:18:26 +0800

    异常处理机制以及自定义提示

---
+++ spring-security-learn/exception/src/main/java/im/dai/HelloController.java
@@ -0,0 +1,23 @@
+package im.dai;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/dai")
+    public String hello() {
+        return "dai";
+    }
+
+    @GetMapping("/admin")
+    public String admin() {
+        return "admin";
+    }
+
+    @GetMapping("/user")
+    public String user() {
+        return "user";
+    }
+}
